8610ac6e90c64cf25e398dc400f58f00639986de
shwetanaren <shweta2911@gmail.com>
2025-03-14 13:19:47 +0100

    implemented transaction feature of knex and verified the result

--- server.js
+++ server.js
@@ -93,29 +93,41 @@ app.post('/signin', (req, res) => {
   });
 });
 
+
   app.post('/register', (req,res) => {
     // extracting the variables through destructuring.
     const { name, email, password }  = req.body;
   
-
   // Set the number of salt rounds (a common default is 10)
   const saltRounds = 10;
 
-  // Hash the plaintext password using bcrypt.hashSync
+  // Hash the plaintext password using bcrypt.hashSync to store in DB
   const hash = bcrypt.hashSync(password, saltRounds);
   console.log('Hashed password:', hash);
-    
-    db('users')
-    .returning('*')
-    .insert({
-     email: email,
-     name: name,
-     joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
+  db.transaction(trx => { //transactions are feature of knex to keep things consistent both fails/successes
+    trx.insert({
+        hash:hash,
+        email:email
     })
-    .catch(err => res.status(400).json(err))
-  });
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+        return trx('users')
+        .returning('*')
+        .insert({
+        email: loginEmail[0].email,
+        name: name,
+        joined: new Date()
+        }).then(user => {
+            res.json(user[0]);
+        })
+    })
+        .then(trx.commit)
+        .catch(trx.callback)
+
+  })  
+  .catch(err => res.status(400).json(err))
+    });
 
 
 app.get('/profile/:id', (req,res) => {
