0383db556c6edbab51537b4babd1ac11df1f2c59
shwetanaren <shweta2911@gmail.com>
2025-03-18 14:16:49 +0100

    brcypt related changes

--- controllers/register.js
+++ controllers/register.js
@@ -1,72 +1,72 @@
-export const handleRegister = async (req, res, db, bcrypt) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-      return res.status(400).json("incorrect form submission");
-    }
-  
-    try {
-      const hash = bcrypt.hashSync(password, 10);
-      console.log("Hashed password:", hash);
-  
-      const newUser = await db.transaction(async trx => {
-        const [loginEmail] = await trx("login")
-          .insert({ hash, email })
-          .returning("email");
-  
-        const [user] = await trx("users")
-          .insert({
-            email: loginEmail,
-            name,
-            joined: new Date(),
-          })
-          .returning("*");
-  
-        return user;
-      });
-  
-      return res.status(201).json(newUser);
-    } catch (err) {
-      console.error("Register error:", err);
-      return res.status(400).json("Unable to register");
-    }
-  };
-
-// export const handleRegister = (req,res, db, bcrypt) => {
-//     // extracting the variables through destructuring.
-//     const { name, email, password }  = req.body;
-//     if(!email || !name || !password) {
-//         return res.status(400).json("incorrect form submission");
-
+// export const handleRegister = async (req, res, db, bcrypt) => {
+//     const { name, email, password } = req.body;
+//     if (!name || !email || !password) {
+//       return res.status(400).json("incorrect form submission");
 //     }
   
-//   // Set the number of salt rounds (a common default is 10)
-//   const saltRounds = 10;
+//     try {
+//       const hash = bcrypt.hashSync(password, 10);
+//       console.log("Hashed password:", hash);
+  
+//       const newUser = await db.transaction(async trx => {
+//         const [loginEmail] = await trx("login")
+//           .insert({ hash, email })
+//           .returning("email");
+  
+//         const [user] = await trx("users")
+//           .insert({
+//             email: loginEmail,
+//             name,
+//             joined: new Date(),
+//           })
+//           .returning("*");
+  
+//         return user;
+//       });
+  
+//       return res.status(201).json(newUser);
+//     } catch (err) {
+//       console.error("Register error:", err);
+//       return res.status(400).json("Unable to register");
+//     }
+//   };
 
-//   // Hash the plaintext password using bcrypt.hashSync to store in DB
-//   const hash = bcrypt.hashSync(password, saltRounds);
-//   console.log('Hashed password:', hash);
-//   db.transaction(trx => { //transactions are feature of knex to keep things consistent both fails/successes
-//     trx.insert({
-//         hash:hash,
-//         email:email
-//     })
-//     .into('login')
-//     .returning('email')
-//     .then(loginEmail => {
-//         return trx('users')
-//         .returning('*')
-//         .insert({
-//         email: loginEmail[0].email,
-//         name: name,
-//         joined: new Date()
-//         }).then(user => {
-//             res.json(user[0]);
-//         })
-//     })
-//         .then(trx.commit)
-//         .catch(trx.callback)
+export const handleRegister = (req,res, db, bcrypt) => {
+    // extracting the variables through destructuring.
+    const { name, email, password }  = req.body;
+    if(!email || !name || !password) {
+        return res.status(400).json("incorrect form submission");
 
-//   })  
-//   .catch(err => res.status(400).json(err))
-//     };
+    }
+  
+  // Set the number of salt rounds (a common default is 10)
+  const saltRounds = bcrypt.genSaltSync(10);
+
+  // Hash the plaintext password using bcrypt.hashSync to store in DB
+  const hash = bcrypt.hashSync(password, saltRounds);
+  console.log('Hashed password:', hash);
+  db.transaction(trx => { //transactions are feature of knex to keep things consistent both fails/successes
+    trx.insert({
+        hash:hash,
+        email:email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+        return trx('users')
+        .returning('*')
+        .insert({
+        email: loginEmail[0].email,
+        name: name,
+        joined: new Date()
+        }).then(user => {
+            res.json(user[0]);
+        })
+    })
+        .then(trx.commit)
+        .catch(trx.rollback)
+
+  })  
+  .catch(err => res.status(400).json("unable to register"))
+    };
 
--- controllers/signin.js
+++ controllers/signin.js
@@ -7,23 +7,22 @@ export const handleSignin = (req, res, db, bcrypt) => {
     }
 
     db.select('email', 'hash').from('login')
-        .where('email', '=', email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if(isValid){
-                console.log(isValid)
-                return db.select('*').from('users')
+    .where('email', '=', email)
+    .then(data => {
+        if (data.length) {   //Checks if user exists
+            const isValid = bcrypt.compareSync(password, data[0].hash); // true
+        if (isValid) {
+            return db.select('*').from('users')
                 .where('email', '=', email)
                 .then(user => {
-                    console.log(user)
                     res.json(user[0])
                 })
-                .catch(err => res.status(400).json('unable to get user'))
-            } else {
-                res.status(400).json('wrong credentials')
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-    };
+                .catch(err => res.status(400).json('Unable to get user'))
+        } else {
+            res.status(400).json('wrong credentials')
+        }
+     }})
+     .catch(err => res.status(400).json('Wrong credentials'))
+};
 
   
